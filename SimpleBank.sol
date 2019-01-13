49b6c9b073326df036f45e15bad838a2932e7136
Ravi <ravidilse@gmail.com>
2019-01-13 08:36:27 +0530

    Updated 13 Jan 2019

--- SimpleBank.sol
+++ SimpleBank.sol
@@ -1,17 +1,16 @@
 // A simple bank smart contract where the user can deposit and query funds.
 pragma solidity >=0.4.22 <0.6.0;
 
-// give your contract a name
 contract SimpleBank {
     
     // this declares a key/value array called balanceAccount.
     // the key is address and the value is an unsigned integer
     mapping (address => uint) balanceAccount;
     address public banker = msg.sender;
-  
-    // declare a deposit function that takes an input called amount
+
+    // Declare a deposit function that takes an input called amount
     function deposit(uint amount) public {
-        require (msg.sender != banker);
+        require (msg.sender == banker, "Only banker can make a deposit");
         // add amount to the balance of the sender
         balanceAccount[msg.sender] += amount;
     }
