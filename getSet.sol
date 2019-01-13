412b4e6281343ede0da08ec218a3c8737a2f3b13
Ravi <ravidilse@gmail.com>
2019-01-13 08:35:35 +0530

    Updated on 13 Jan 2019

--- getSet.sol
+++ getSet.sol
@@ -1,16 +1,18 @@
-pragma solidity >=0.4.22 <0.6.0;
 //pragma solidity ^0.4.0;
+pragma solidity >=0.4.22 <0.6.0;
 
+// give your contract a name
 contract getSet {
     uint age;
     string name;
     
-    
+    // declare a function that accept an input    
     function setValue(string memory _name, uint _age) public {
         age = _age;
         name = _name;
     }
-    
+
+    // declare a function that return an output    
     function getValue() public view returns(string memory, uint) {
         return (name, age);
     }
